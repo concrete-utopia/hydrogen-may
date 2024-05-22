eafced388f3e0c466ff81058444939781ced2312
Balint Gabor <127662+gbalint@users.noreply.github.com>
2024-05-22 14:41:50 +0200

    Fix product undefined error

--- app/components/hydrogen/ProductCard.jsx
+++ app/components/hydrogen/ProductCard.jsx
@@ -7,33 +7,6 @@ import {Price, PriceCompareAt} from './Price';
 import {cx} from './new/cva.config';
 
 export function ProductCard({product, className, loading, onClick, quickAdd}) {
-  product = {
-    handle: 'builders-tote',
-    title: 'Builders Tote',
-    variants: {
-      nodes: [
-        {
-          availableForSale: true,
-          price: {
-            amount: '38.00',
-            currencyCode: 'CAD',
-          },
-          compareAtPrice: {
-            amount: '42.00',
-            currencyCode: 'CAD',
-          },
-          image: {
-            id: 'gid://Shopify/placeholder/1234',
-            altText: 'Placeholder',
-            height: '600',
-            width: '400',
-            url: 'https://cdn.shopify.com/s/files/1/0657/3811/3197/files/builders-tote.png',
-          },
-        },
-      ],
-    },
-  };
-
   if (!product?.variants?.nodes?.length) return null;
 
   const firstVariant = flattenConnection(product.variants)[0];
--- app/routes/_index/sections/best-sellers.jsx
+++ app/routes/_index/sections/best-sellers.jsx
@@ -3,6 +3,32 @@ import {Heading} from '@h2/Text';
 import {Container, Grid, Section} from '@h2/new/Layout';
 
 export default function BestSellers() {
+  const product = {
+    handle: 'builders-tote',
+    title: 'Builders Tote',
+    variants: {
+      nodes: [
+        {
+          availableForSale: true,
+          price: {
+            amount: '38.00',
+            currencyCode: 'CAD',
+          },
+          compareAtPrice: {
+            amount: '42.00',
+            currencyCode: 'CAD',
+          },
+          image: {
+            id: 'gid://Shopify/placeholder/1234',
+            altText: 'Placeholder',
+            height: '600',
+            width: '400',
+            url: 'https://cdn.shopify.com/s/files/1/0657/3811/3197/files/builders-tote.png',
+          },
+        },
+      ],
+    },
+  };
   return (
     <Section className="py-32">
       <Container as="header">
@@ -13,12 +39,30 @@ export default function BestSellers() {
       </Container>
       <Container>
         <Grid columns={12} rows={12} className="aspect-[4/6]">
-          <ProductCard className="col-span-5 col-start-1 row-start-1" />
-          <ProductCard className="col-span-3 col-start-7 row-start-3" />
-          <ProductCard className="col-span-3 col-start-10 row-start-1" />
-          <ProductCard className="col-span-3 col-start-1 row-start-9" />
-          <ProductCard className="col-span-3 col-start-5 row-start-7" />
-          <ProductCard className="col-span-4 col-start-9 row-start-8" />
+          <ProductCard
+            className="col-span-5 col-start-1 row-start-1"
+            product={product}
+          />
+          <ProductCard
+            className="col-span-3 col-start-7 row-start-3"
+            product={product}
+          />
+          <ProductCard
+            className="col-span-3 col-start-10 row-start-1"
+            product={product}
+          />
+          <ProductCard
+            className="col-span-3 col-start-1 row-start-9"
+            product={product}
+          />
+          <ProductCard
+            className="col-span-3 col-start-5 row-start-7"
+            product={product}
+          />
+          <ProductCard
+            className="col-span-4 col-start-9 row-start-8"
+            product={product}
+          />
         </Grid>
       </Container>
     </Section>
