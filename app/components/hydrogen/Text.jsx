fe6b53af40dc036e26f69542df4155f673d54b1b
RheeseyB <1044774+Rheeseyb@users.noreply.github.com>
2024-05-22 16:14:48 +0100

    Remove white-space pre-wrap

--- app/components/hydrogen/Text.jsx
+++ app/components/hydrogen/Text.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import clsx from 'clsx';
-import {missingClass, formatText} from '~/lib/utils';
+import clsx from 'clsx'
+import { missingClass, formatText } from '~/lib/utils'
 
 export function Text({
   as: Component = 'span',
@@ -18,33 +18,32 @@ export function Text({
     subtle: 'text-primary/50',
     accent: 'text-accent',
     contrast: 'text-contrast/90',
-  };
+  }
 
   const sizes = {
     lead: 'text-lead font-medium',
     copy: 'text-copy',
     fine: 'text-fine subpixel-antialiased',
-  };
+  }
 
   const widths = {
     default: 'max-w-prose',
     narrow: 'max-w-prose-narrow',
     wide: 'max-w-prose-wide',
-  };
+  }
 
   const styles = clsx(
     missingClass(className, 'max-w-') && widths[width],
-    missingClass(className, 'whitespace-') && 'whitespace-pre-wrap',
     missingClass(className, 'text-') && colors[color],
     sizes[size],
     className,
-  );
+  )
 
   return (
     <Component {...props} className={styles}>
       {format ? formatText(children) : children}
     </Component>
-  );
+  )
 }
 
 export function Heading({
@@ -61,24 +60,23 @@ export function Heading({
     heading: 'font-bold text-heading',
     lead: 'font-bold text-lead',
     copy: 'font-medium text-copy',
-  };
+  }
 
   const widths = {
     default: 'max-w-prose',
     narrow: 'max-w-prose-narrow',
     wide: 'max-w-prose-wide',
-  };
+  }
 
   const styles = clsx(
-    missingClass(className, 'whitespace-') && 'whitespace-pre-wrap',
     missingClass(className, 'max-w-') && widths[width],
     missingClass(className, 'font-') && sizes[size],
     className,
-  );
+  )
 
   return (
     <Component {...props} className={styles}>
       {format ? formatText(children) : children}
     </Component>
-  );
+  )
 }
