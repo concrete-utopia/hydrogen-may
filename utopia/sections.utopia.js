cf3c594af87e78bf9875fcb7616c03bd3fee1cea
Balint Gabor <127662+gbalint@users.noreply.github.com>
2024-05-24 17:29:52 +0200

    Add annotations for the section components

---
+++ utopia/sections.utopia.js
@@ -0,0 +1,51 @@
+import * as Utopia from 'utopia-api'
+import Hero from '/app/routes/_index/sections/hero'
+import BestSellers from '/app/routes/_index/sections/best-sellers'
+import Collections from '/app/routes/_index/sections/collections'
+import FeaturedProducts from '/app/routes/_index/sections/featured-products'
+import OurPromise from '/app/routes/_index/sections/our-promise'
+
+const Components = {
+  '/app/routes/_index/sections/hero': {
+    Hero: {
+      component: Hero,
+      properties: {},
+      inspector: 'hidden',
+      focus: 'always',
+    },
+  },
+  '/app/routes/_index/sections/best-sellers': {
+    BestSellers: {
+      component: BestSellers,
+      properties: {},
+      inspector: 'hidden',
+      focus: 'always',
+    },
+  },
+  '/app/routes/_index/sections/collections': {
+    Collections: {
+      component: Collections,
+      properties: {},
+      inspector: 'hidden',
+      focus: 'always',
+    },
+  },
+  '/app/routes/_index/sections/featured-products': {
+    FeaturedProducts: {
+      component: FeaturedProducts,
+      properties: {},
+      inspector: 'hidden',
+      focus: 'always',
+    },
+  },
+  '/app/routes/_index/sections/our-promise': {
+    OurPromise: {
+      component: OurPromise,
+      properties: {},
+      inspector: 'hidden',
+      focus: 'always',
+    },
+  },
+}
+
+export default Components
