5f3a8ce84bd70434a41f4689df894c95a40a3a3a
Benjamin Sehl <ben.sehl@shopify.com>
2024-05-25 23:37:44 -0400

    homepage tweaks

--- app/routes/_index/sections/hero.jsx
+++ app/routes/_index/sections/hero.jsx
@@ -1,20 +1,28 @@
-import {Button} from '@h2/Button';
-import {Heading, Text} from '@h2/new/Text';
+import {Button} from '@h2/new/Button';
+import {Heading, Text, Span} from '@h2/new/Text';
 import {Background, Container, Flex, Section} from '@h2/new/Layout';
 import {Image} from '@shopify/hydrogen';
 
 export default function Hero() {
   return (
-    <Section className="aspect-video bg-darkGray">
+    <Section className="aspect-[8/5] bg-darkGray">
       <Container resizeY="fill" className="py-24">
         <Flex direction="down" resizeY="fill" justify="between" align="start">
-          <Flex direction="down">
-            <Text uppercase size={6} className="text-accent font-display">
+          <Flex direction="down" gap="s">
+            <Text
+              uppercase
+              size={6}
+              font="display"
+              color="accent"
+              className="pl-1"
+            >
               Summer 2024
             </Text>
-            <Heading size="8" color="white">
+            <Heading font="text" size="8" color="white">
               Building <br />
-              <span className="uppercase font-display">Essentials</span>
+              <Span uppercase font="display">
+                Essentials
+              </Span>
             </Heading>
           </Flex>
           <Button to="/">Shop now</Button>
@@ -26,7 +34,7 @@ export default function Hero() {
           <div className="absolute inset-0 bg-gradient-to-r from-darkGray to-transparent to-50% z-0"></div>
           <Image
             src="https://cdn.shopify.com/s/files/1/0657/3811/3197/files/builders-tote.png"
-            className="w-full h-full -z-10"
+            className="object-cover w-full h-full -z-10"
           />
         </div>
       </Background>
