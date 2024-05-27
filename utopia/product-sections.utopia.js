61af50a089c45664f97c6ad710822adfb485e611
Balint Gabor <127662+gbalint@users.noreply.github.com>
2024-05-27 19:01:51 +0200

    Fix copy paste error

--- utopia/product-sections.utopia.js
+++ utopia/product-sections.utopia.js
@@ -3,7 +3,7 @@ import Hero from '/app/routes/products.$handle/sections/hero';
 import HighlightDetails from '/app/routes/products.$handle/sections/highlight-details';
 import HighlightSolution from '/app/routes/products.$handle/sections/highlight-solution';
 import Recommended from '/app/routes/products.$handle/sections/recommended';
-import Reviews from '/app/routes/products.$handle/sections/reviews';
+import Reviews, {Review} from '/app/routes/products.$handle/sections/reviews';
 import Spotlight from '/app/routes/products.$handle/sections/spotlight';
 
 const Components = {
@@ -47,70 +47,6 @@ const Components = {
       focus: 'always',
     },
   },
-  '/app/routes/products.$handle/sections/spotlight': {
-    Spotlight: {
-      component: Spotlight,
-      properties: {},
-      inspector: 'hidden',
-      focus: 'always',
-    },
-  },
-};
-
-export default Components;
-import * as Utopia from 'utopia-api'
-import Hero from '/app/routes/products.$handle/sections/hero'
-import HighlightDetails from '/app/routes/products.$handle/sections/highlight-details'
-import HighlightSolution from '/app/routes/products.$handle/sections/highlight-solution'
-import Recommended from '/app/routes/products.$handle/sections/recommended'
-import Reviews, {
-  Review,
-} from '/app/routes/products.$handle/sections/reviews'
-import Spotlight from '/app/routes/products.$handle/sections/spotlight'
-
-const Components = {
-  '/app/routes/products.$handle/sections/hero': {
-    Hero: {
-      component: Hero,
-      properties: {},
-      inspector: 'hidden',
-      focus: 'always',
-    },
-  },
-  '/app/routes/products.$handle/sections/highlight-details':
-    {
-      HighlightDetails: {
-        component: HighlightDetails,
-        properties: {},
-        inspector: 'hidden',
-        focus: 'always',
-      },
-    },
-  '/app/routes/products.$handle/sections/highlight-solution':
-    {
-      HighlightSolution: {
-        component: HighlightSolution,
-        properties: {},
-        inspector: 'hidden',
-        focus: 'always',
-      },
-    },
-  '/app/routes/products.$handle/sections/recommended': {
-    Recommended: {
-      component: Recommended,
-      properties: {},
-      inspector: 'hidden',
-      focus: 'always',
-    },
-  },
-  '/app/routes/products.$handle/sections/reviews': {
-    Reviews: {
-      component: Reviews,
-      properties: {},
-      inspector: 'hidden',
-      focus: 'always',
-    },
-  },
   '/app/routes/products.$handle/sections/reviews': {
     Review: {
       component: Review,
@@ -132,6 +68,6 @@ const Components = {
       focus: 'always',
     },
   },
-}
+};
 
-export default Components
+export default Components;
