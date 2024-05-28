8170307fbe19bfd09cc938136f17d08664a3c3da
Balint Gabor <127662+gbalint@users.noreply.github.com>
2024-05-28 10:34:15 +0200

    Fix best sellers hardcoded handle

--- app/routes/_index/sections/best-sellers.jsx
+++ app/routes/_index/sections/best-sellers.jsx
@@ -1,11 +1,16 @@
-import {ProductCard} from '@h2/ProductCard';
-import {Heading, Span} from '@h2/new/Text';
-import {Container, Flex, Grid, Section} from '@h2/new/Layout';
+import { ProductCard } from '@h2/ProductCard'
+import { Heading, Span } from '@h2/new/Text'
+import {
+  Container,
+  Flex,
+  Grid,
+  Section,
+} from '@h2/new/Layout'
 
 export default function BestSellers() {
   const product = {
-    handle: 'builders-tote',
-    title: 'Builders Tote',
+    handle: 'builder-tote',
+    title: 'Builder Tote',
     variants: {
       nodes: [
         {
@@ -28,52 +33,64 @@ export default function BestSellers() {
         },
       ],
     },
-  };
+  }
   return (
-    <Section className="pt-32">
-      <Container as="header" className="z-20 pointer-events-none">
+    <Section className='pt-32'>
+      <Container
+        as='header'
+        className='z-20 pointer-events-none'
+      >
         <Heading
-          weight="regular"
+          weight='regular'
           uppercase
           size={8}
-          className="relative -mx-1 -mb-8"
+          className='relative -mx-1 -mb-8'
         >
-          <Flex as="span" justify="between">
-            <Span font="display">Best</Span>
-            <Span font="sans" className="-mr-2 scale-95 translate-y-1">
+          <Flex as='span' justify='between'>
+            <Span font='display'>Best</Span>
+            <Span
+              font='sans'
+              className='-mr-2 scale-95 translate-y-1'
+            >
               Sellers
             </Span>
           </Flex>
         </Heading>
       </Container>
       <Container>
-        <Grid columns={12} rows={10} gapY="xl" gap="l" className="aspect-[5/7]">
+        <Grid
+          columns={12}
+          rows={10}
+          gapY='xl'
+          gap='l'
+          className='aspect-[5/7]'
+        >
           <ProductCard
-            className="col-span-5 col-start-1 row-start-1 pr-12"
+            className='col-span-5 col-start-1 row-start-1 pr-12'
             product={product}
           />
           <ProductCard
-            className="col-span-3 col-start-7 row-start-2"
+            className='col-span-3 col-start-7 row-start-2'
             product={product}
           />
           <ProductCard
-            className="col-span-3 col-start-10 row-start-1"
+            className='col-span-3 col-start-10 row-start-1'
             product={product}
           />
           <ProductCard
-            className="col-span-3 col-start-1 row-start-6"
+            className='col-span-3 col-start-1 row-start-6'
             product={product}
           />
           <ProductCard
-            className="col-span-3 col-start-4 row-start-5"
+            className='col-span-3 col-start-4 row-start-5'
             product={product}
           />
           <ProductCard
-            className="col-span-4 col-start-9 row-start-5 mt-8"
+            className='col-span-4 col-start-9 row-start-5 mt-8'
             product={product}
           />
         </Grid>
       </Container>
     </Section>
-  );
+  )
 }
